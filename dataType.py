80b4b536ed6d56f298e1008dc27d0ef41eeec247
shahin2525 <shahin451999@gmail.com>
2024-03-25 01:26:28 -0700

    syntax added

--- dataType.py
+++ dataType.py
@@ -2,4 +2,15 @@ name = "rakib"
 age = 20
 id= "admin123"
 fathersName = "sakib khan"
-print(fathersName)
+x = str(3)
+y = int(3)
+z = float(3)
+i = 12j
+a = 4
+A = "Sally"
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(i))
+print(a)
+print(A)
---
+++ syntax.py
@@ -0,0 +1,12 @@
+x = 2
+y=2.1
+z = 2j
+a = float(x)
+b = complex(y)
+c = str(z)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
