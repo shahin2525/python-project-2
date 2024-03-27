b844efa581b1c6f69d196607d9cad698171751d9
shahin2525 <shahin451999@gmail.com>
2024-03-26 20:43:31 -0700

    string format added

---
+++ formatString.py
@@ -0,0 +1,5 @@
+num = 10
+num2=20
+
+# print(f'my number is {num+num2}')
+print(f'my number is {num+num2} ')
--- stringType.py
+++ stringType.py
@@ -1,6 +1,13 @@
 myName = "shahin"
 #
 # print("amar nam holo 2"+ " "+ myName)
-x = 10
-y=10
-print(x==y)
+# x = 10
+# y=10
+# print(x==y)
+i = 5
+a = 4
+b = 6.1
+d = "61.2"
+x = int(b)
+print(type(x))
+
