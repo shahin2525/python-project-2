c6fb715fe11a1c0cdeecd98511859bec0caaed8e
shahin2525 <shahin451999@gmail.com>
2024-03-28 23:52:07 -0700

    syqunce type data added

--- booleanType.py
+++ booleanType.py
@@ -1,12 +1,25 @@
-x = b"hello"
-print(type(x))
-# bytearry
-# i = [1,2,3,4]
+# x = b"hello"
+# print(type(x))
+# # bytearry
+# # i = [1,2,3,4]
+# # j = bytearray(i)
+# # # j[2]=8
+# #
+# # print(j[2])
+# i = [1,3,4,7]
 # j = bytearray(i)
-# # j[2]=8
+# # j[1]=25
+# print(j[1])
 #
-# print(j[2])
-i = [1,3,4,7]
-j = bytearray(i)
-# j[1]=25
-print(j[1])
+# x = None
+# y = ""
+# print(type(y))
+
+li = [1,2,3,5]
+li[1]=20
+
+tuple = (1,2,3,8)
+ran =range (6)
+for i in ran:
+   print(i)
+
