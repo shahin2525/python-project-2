dcd6f2327dfb87422860e46bf9684806ae4f807d
shahin2525 <shahin451999@gmail.com>
2024-04-11 21:43:39 -0700

    list remove practice added

--- type.py
+++ type.py
@@ -4,7 +4,14 @@ isActive = [True,False,True,False]
 isActive[1]=True
 hablu = ["channel","website","linkdin"]
 hablu[2]="indeed"
-listItems = ["cherry0","apple","banana"]
-listItems.append("orange")
-listItems.insert(0,"pear")
+listItems = ["cherry0","apple","banana",20]
+# listItems.append("orange")
+# listItems.insert(0,"pear")
+
+# listItems.remove(20)
+# listItems.pop(1)
+# listItems.pop()
+# del  listItems[2]
+listItems.clear()
+
 print(listItems)
